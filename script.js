cd22443e6972a91a2f315c888765b19bbea37a6b
Ankita <anki333soul@gmail.com>
2021-09-16 23:29:53 -0700

    set localStorage

--- script.js
+++ script.js
@@ -17,7 +17,9 @@ let backlogListArray = [];
 let progressListArray = [];
 let completeListArray = [];
 let onHoldListArray = [];
+let listArrays = [];
 
+console.log(backlogListArray);
 // Drag Functionality
 
 
@@ -36,12 +38,18 @@ function getSavedColumns() {
   }
 }
 
+getSavedColumns();
+updateSavedColumns();
+
 // Set localStorage Arrays
 function updateSavedColumns() {
-  localStorage.setItem('backlogItems', JSON.stringify(backlogListArray));
-  localStorage.setItem('progressItems', JSON.stringify(progressListArray));
-  localStorage.setItem('completeItems', JSON.stringify(completeListArray));
-  localStorage.setItem('onHoldItems', JSON.stringify(onHoldListArray));
+  listArrays = [backlogListArray, progressListArray, completeListArray,   onHoldListArray];
+
+  const arrayNames = ['backlog', 'progress', 'complete', 'onHold'];
+  arrayNames.forEach((arrayName, index) => {
+    localStorage.setItem(`${arrayName}Items`, JSON.stringify(listArrays[index]));
+  });
+  console.log(arrayNames);
 }
 
 // Create DOM Elements for each list item
